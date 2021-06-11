d8827dac0003075a42f4df5d50f1770a8e879921
Tom Bushart <tbushart@bp-3.com>
2021-06-10 21:02:21 -0400

    Add internal code for matrix inversion caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to either calculate or return the previously calculated
+## inversion of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix accepts a matrix value and can store its calculated inverse
+## Pass it a matrix for intial setting. Use its set method to change the stored matrix.
+## getInverse and setInverse are used by cacheSolve for caching
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  cachedInverse <- NULL
 
+  set <- function(newMatrix) {
+    x <<- newMatrix
+    cachedInverse <<- NULL
+  }
+  get <- function() {
+    return(x)
+  }
+  setInverse <- function(inverse) {
+    cachedInverse <<- inverse
+  }
+  getInverse <- function() {
+    return(cachedInverse)
+  }
+
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a makeCacheMatrix argument and calculates its inverse or
+## returns a previously calculated inverse for the same matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("getting cached inversion")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  return(inverse)
 }
+
+
